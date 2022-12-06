93ff8d90919852324b4519325447fa51c327311f
izzywest <misseasybella@gmail.com>
2022-12-06 20:15:41 +0100

    add calculation and formula

--- src/main/java/Controller/ImperialController.java
+++ src/main/java/Controller/ImperialController.java
@@ -1,4 +1,19 @@
 package Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class ImperialController {
+
+    @GetMapping("/api/calcCentimeterFromInches")
+
+    public double calcCentimeterFromInches(@GetMapping double inch) {
+        return inch*2.54;
+    }
+
+    @GetMapping("/api/calcMeterFromYard")
+
+    public double calcMeterFromYard(@GetMapping double yard) {
+        return yard / 1.094;
+    }
+
 }
