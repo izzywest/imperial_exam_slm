eff225d040ec6eae9c913e82ba8fe96c89b2afdb
izzywest <misseasybella@gmail.com>
2022-12-06 19:37:37 +0100

    Initial commit

---
+++ src/main/java/at/fhtechnikum/imperial/ImperialApplication.java
@@ -0,0 +1,13 @@
+package at.fhtechnikum.imperial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImperialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhtechnikum/imperial/ImperialApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtechnikum.imperial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImperialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
